b5b4ce0365ae64a8201bb83b1b30cc15c33afe56
denisCenter2m <47447276+DenisYavetsky@users.noreply.github.com>
2021-08-14 23:26:31 +0300

    project works

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def main(request):
+    return render(request, 'products/index.html')
+
+
+def products(request):
+    return render(request, 'products/products.html')
+
+
+def contact(request):
+    return render(request, 'products/contact.html')
